cd687d969cf85f6f2967d0355a4bb2eb59f192fb
adinkramedia <adinkraaudio@outlook.com>
2025-10-05 22:17:47 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,11 @@ import HouseOfAusar from "./pages/HouseOfAusar";
 import HouseArticle from "./pages/HouseArticle";
 import News from "./pages/News";
 import NewsArticle from "./pages/NewsArticle";
-import Apply from "./pages/Apply";
-import Dashboard from "./pages/Dashboard";
-import EditProfile from "./pages/EditProfile";
-import SubmitArticle from "./pages/SubmitArticle";
 import AdinkraTV from "./pages/AdinkraTV";
 import TVVideoPage from "./pages/TVVideoPage";
 import PremiumTV from "./pages/PremiumTV";
 import PremiumVideo from "./pages/PremiumVideo";
 import Contact from "./pages/Contact";
-import ShareDashboard from "./pages/ShareDashboard";
 
 // Games
 import Games from "./Games/Games";
@@ -44,21 +39,13 @@ export default function App() {
 
         {/* Page Content */}
         <div className="flex-1 relative pt-20">
-          {/* added pt-20 to push content below sticky header */}
           <Routes>
             {/* Core Pages */}
             <Route path="/" element={<Home />} />
             <Route path="/audio" element={<Audio />} />
             <Route path="/house-of-ausar" element={<HouseOfAusar />} />
             <Route path="/news" element={<News />} />
-            <Route path="/apply" element={<Apply />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/edit-profile" element={<EditProfile />} />
-            <Route path="/submit-article" element={<SubmitArticle />} />
             <Route path="/contact" element={<Contact />} />
-
-            {/* Public Contributor Profile */}
-            <Route path="/contributor/:id" element={<ShareDashboard />} />
 
             {/* Article Pages */}
             <Route path="/house-article/:id" element={<HouseArticle />} />
